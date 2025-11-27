5e48691a774e494accf6d5539a622aaed9ba7768
Melo <102263606+Melo1337@users.noreply.github.com>
2025-11-27 12:34:02 -0300

    UPDATE DATA

--- data/products.js
+++ data/products.js
@@ -2,25 +2,25 @@ export const listaDeProdutos = [
     {
         nome: "Brother MFC-9330CDW",
         preco: 2500.00,
-        descricao: "Multifuncional semi-nova, laser, Colorida, wif-fi",
+        descricao: "Multifuncional semi-nova, laser, Colorida, wif-fi, USB",
         imagemSrc: "/images/imp1.jpg" 
     },
     {
         nome: "Brother Mfc-9460CDN",
         preco: 2000.00,
-        descricao: "Multifuncional semi-nova, laser, Colorida",
+        descricao: "Multifuncional semi-nova, laser, Colorida, USB",
         imagemSrc: "/images/imp2.jpg"
     },
     {
         nome: "HP LaserJet Color CP1215",
         preco: 800.00,
-        descricao: "Impressora semi-nova, laser, Colorida, wif-fi",
+        descricao: "Impressora semi-nova, laser, Colorida, USB",
         imagemSrc: "/images/imp3.jpg"
     },
     {
         nome: "Brother DCP-8157DN",
         preco: 1000.00,
-        descricao: "Multifuncional semi-nova, laser",
+        descricao: "Multifuncional semi-nova, laser, USB",
         imagemSrc: "/images/imp4.jpg"
     },
 ];
