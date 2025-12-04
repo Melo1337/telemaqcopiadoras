4d2e8988987249507cf238f446a7ed1ae4cac9ab
Melo <102263606+Melo1337@users.noreply.github.com>
2025-12-04 17:18:59 -0300

    init

---
+++ js/printerFeatures.js
@@ -0,0 +1,6 @@
+const classContainerPrint = document.querySelector(".container-print")
+const classPrinter = document.querySelector(".printer")
+
+classPrinter.addEventListener("click", function() {
+  console.log("hello world!!!");
+});
