4a3a2462288a4f2829ef2cd4487ec7ed00448a45
Melo <102263606+Melo1337@users.noreply.github.com>
2025-12-04 21:10:16 +0000

    .

--- js/printerFeatures.js
+++
@@ -1,8 +0,0 @@
-const classContainerPrint = document.querySelector(".container-print")
-const classPrinter = document.querySelectorAll(".printer")
-
-classPrinter.forEach(printer => {
-    printer.addEventListener("click", function () {
-        window.location.href = 'printerFeatures.html'
-    });
-})
