4a1a4da506d9e17932823ad138e5c8ddfd4745f5
Melo <102263606+Melo1337@users.noreply.github.com>
2025-12-11 18:53:00 -0300

    .

--- js/loadPrinterFeatures.js
+++ js/loadPrinterFeatures.js
@@ -1,8 +1,11 @@
 import {getProdutos} from '../data/requestProducts.js'
 
 document.addEventListener('DOMContentLoaded', async () => {
+    
     const containerPrinter = document.querySelector('.container-printer');
-    let idDiv = localStorage.getItem('idDiv');
+    const idDiv = localStorage.getItem('idDiv');
+    const listaDeProdutos = await getProdutos();
+    const produto = listaDeProdutos[idDiv];
 
     function loadHeadFeatures(headFeatures) {
         return headFeatures
@@ -10,9 +13,23 @@ document.addEventListener('DOMContentLoaded', async () => {
             .join("");
     }
 
-    // Carregar produto selecionado
-    const listaDeProdutos = await getProdutos();
-    const produto = listaDeProdutos[idDiv];
+    function loadFeatures() {
+
+        console.log(produto.feature);
+        
+
+        return `<div class="feature">
+                <div class="btn-features">
+                    <p>IMPRESSÃO</p>
+                    <img src="./images/arrow.png" alt="">
+                </div>
+                <table>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>`
+    }
 
     if (containerPrinter && produto) {
         containerPrinter.innerHTML = `
@@ -27,47 +44,14 @@ document.addEventListener('DOMContentLoaded', async () => {
                 <a href="https://wa.me/5532984195001?text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20um%20or%C3%A7amento%20de%20loca%C3%A7%C3%A3o!">
                     <div class="btn whatsapp">
                         <img src="./images/whatsapp.png" alt="whatsapp-icon">
-                        <h4>Solicitar Orçamento</h4>
+                        <h4>Comprar via whatsapp</h4>
                     </div>
                 </a>
             </div>
         </div>
 
         <div class="container-main">
-            <div class="feature1">
-                <div class="btn-features">
-                    <p>IMPRESSÃO</p>
-                    <img src="./images/arrow.png" alt="">
-                </div>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>Padrão de impressão frente e verso</th>
-                            <td>Sim</td>
-                        </tr>
-                        <tr>
-                            <th>Velocidade de impressão em preto A4 (max. ppm)</th>
-                            <td>40</td>
-                        </tr>
-                        <tr>
-                            <th>Tipo de consumível</th>
-                            <td>Cartucho de toner e unidade de cilindro</td>
-                        </tr>
-                        <tr>
-                            <th>Resolução de impressão (dpi)</th>
-                            <td>1200 x 1200 dpi</td>
-                        </tr>
-                        <tr>
-                            <th>Tecnologia de impressão</th>
-                            <td>Monocromática</td>
-                        </tr>
-                        <tr>
-                            <th>Compatibilidade com o driver de impressora</th>
-                            <td>Windows, Mac OS, Linux</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            ${loadFeatures()}
         </div>`;
     }
 });
